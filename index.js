2c42196d760e9c9fcffbdb6359a052fd1f18bd45
Sumanth Devadiga <sdevadiga@technosofteng.com>
2024-03-20 13:08:37 +0530

    test api

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+// const mongoose = require('mongoose');
+const cors = require('cors');
+// require('dotenv').config()
+const PORT = 5000
+const serverless = require("serverless-http");
+
+
+const app = express();
+
+
+app.use(cors())
+app.use(express.json())
+
+
+// API Endpoints
+
+app.get('/',(req,res)=>res.send("Im alive"))
+app.post('/data',async(req,res)=>{
+    console.log(req.body);
+    return res.json({
+        success: true,
+        data : req.body
+    })
+})     
+
+
+// Express and Mongo Connection
+const start = async () => {
+    try {
+    //   await mongoose.connect(
+    //     "mongodb://localhost:27017/UserManagement"
+    //   );
+      app.listen(PORT, () => console.log(`Server started on port at :- http://localhost:${PORT}`));
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  };
+  
+start();
